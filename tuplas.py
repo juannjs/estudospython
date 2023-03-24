689d159aa4922dcdf5b9b1837c4292b0e43b3671
Juan Bastos <j0badonly@gmail.com>
2023-03-24 05:09:14 -0300

    alteração final

---
+++ tuplas.py
@@ -0,0 +1,17 @@
+#Exemplo de lista principal
+tuplas=("thiago","python","udemy")
+
+#Para saber qual variável está em qual posição, usamos o "index" como ensinado anteriormente
+print(tuplas[0:2])
+
+#Para saber a quantidade de coisas que tem na lista, usamos "len"
+print(len(tuplas))
+
+#Também da pra somar as listas usando o exemplo abaixo
+print(tuplas+tuplas)
+
+#Também da para multiplicar usando o exemplo abaixo
+print(tuplas*5)
+
+#Para verificar se há algo em especifico dentro da lista, usasse o exemplo abaixo
+print("udemy" in tuplas)
